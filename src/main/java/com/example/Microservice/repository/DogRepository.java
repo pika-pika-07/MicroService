cbdd266e7ddec75d07e14491659186da9c98a880
parth bhatnagar <parthbhatnagar@192.168.1.4>
2021-04-18 17:03:17 +0530

    Added dog repository

---
+++ src/main/java/com/example/Microservice/repository/DogRepository.java
@@ -0,0 +1,8 @@
+package com.example.Microservice.repository;
+
+import com.example.Microservice.entity.Dog;
+import org.springframework.data.repository.CrudRepository;
+
+public interface DogRepository extends CrudRepository<Dog,Long> {
+    
+}
